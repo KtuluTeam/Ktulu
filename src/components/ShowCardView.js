e78b966de02cbe237eea8da0744153021134e7f1
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-05-06 16:02:47 +0200

    Remember user names

--- src/components/ShowCardView.js
+++ src/components/ShowCardView.js
@@ -2,21 +2,24 @@ import React from 'react'
 import {
   Text,
   View,
-  Button
+  Button,
+  TextInput
 } from 'react-native'
+import * as cardInfo from '../cards'
 
-export const ShowCardView = ({number, index, cards, onSubmit}) => {
-  console.log("rendering ShowCardView")
-  let card = cards[index].role
+export const ShowCardView = ({number, index, cards, onSubmit, onChangeName}) => {
+  let card = cards[index]
   return (
     <View>
-    <Text>
-    {index}
-    {card}
-    </Text>
-    <Text>
-    Podaj imię:
-    </Text>
+      <Text>
+        {cardInfo[card.faction][card.role].name}
+      </Text>
+      <Text>
+        Podaj imię:
+      </Text>
+      <TextInput
+        value={card.name}
+        onChangeText={onChangeName} />
       <Button title="Zatwierdź i ukryj kartę" onPress={onSubmit} />
     </View>
   );
