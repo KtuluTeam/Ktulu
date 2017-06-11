82af80a15610a22a760021ad75f2fcd7e03975f1
Michał Naruniec <mn360386@students.mimuw.edu.pl>
2017-06-11 16:15:03 +0200

    YesNo inline style

--- src/components/ChoiceView.js
+++ src/components/ChoiceView.js
@@ -2,24 +2,29 @@ import React from 'react'
 import {
   Text,
   View,
-  Button,
-  Picker
+  TouchableOpacity,
+  Image
 } from 'react-native'
 import { Top } from './index'
 
-let yesImg = require("../../img/yes.png")
+let yesImg = require('../../img/icons/yes.png')
+let noImg = require('../../img/icons/no.png')
 
 export const ChoiceView = ({onMenu, statueHolder, onYes, onNo, text}) => {
   return (
     <View>
-    <Top statueHolder={statueHolder} onMenu={onMenu}/>
-    <Text> {text} </Text>
-    <TouchableOpacity onPress={onYes}>
-      <Image source={yesImg} style={{width:200, height:200}} />
-    </TouchableOpacity>
-    <Button title="NIE"  onPress={onNo} />
+      <Top statueHolder={statueHolder} onMenu={onMenu} />
+      <Text> {text} </Text>
+      <View style={{flexDirection: 'row', bottom: 0}} >
+        <TouchableOpacity onPress={onYes} style={{width: 150}} >
+          <Image source={yesImg} style={{width: 100, height: 100, left: 50}} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onNo} style={{width: 150}}>
+          <Image source={noImg} style={{width: 100, height: 100, left: 50}} />
+        </TouchableOpacity>
+      </View>
     </View>
-  );
+  )
 }
 
 export default ChoiceView
