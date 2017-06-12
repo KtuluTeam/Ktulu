ba2b852c0f2ebfd429806cbc4e29a7587c581dad
klaudialax <klaudialax@gmail.com>
2017-06-12 23:02:55 +0200

    views changes fix

--- src/components/WakeUpByNameView.js
+++ src/components/WakeUpByNameView.js
@@ -13,7 +13,7 @@ export const WakeUpByNameView = ({text, onMenu, statueHolder, onSubmit, who}) =>
     <View>
       <Top statueHolder={statueHolder} onMenu={onMenu} />
       <ReadLoud text={text} />
-      <ManitouInfo text={'Obudź uczestnika: ' + who.name + 'POPRZEZ DOTKNIĘCIE'} />
+      <ManitouInfo text={'Obudź uczestnika: ' + who.name + ' POPRZEZ DOTKNIĘCIE'} />
       <NextFooter title='OK' onPress={onSubmit} />
     </View>
   )
