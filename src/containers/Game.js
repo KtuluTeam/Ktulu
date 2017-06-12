74f80c7978ad6a0e5884d9ebec6d3d79ae025ee5
Michał Naruniec <mn360386@students.mimuw.edu.pl>
2017-06-12 16:39:56 +0200

    Test backgound color

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,12 +1,22 @@
+import React from 'react'
+import {
+  View
+} from 'react-native'
 import { connect } from 'react-redux'
 import { viewMap } from '../containers'
 
 let GameView = ({stage, step, substep}) => {
+  let GameScreen
   if (viewMap[stage][step].hasSubsteps === undefined) {
-    return viewMap[stage][step]()
+    GameScreen = viewMap[stage][step]()
   } else {
-    return viewMap[stage][step][substep]()
+    GameScreen = viewMap[stage][step][substep]()
   }
+  return (
+    <View>
+      <GameScreen />
+    </View>
+  )
 }
 
 let mapStateToProps = ({stage, step, substep}) => {
